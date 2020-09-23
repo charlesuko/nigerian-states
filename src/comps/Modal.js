6165e24fd9af143b7009a73d8155d6239da63904
Charles Uko <charlesuko@Charless-MBP.lan>
2020-09-23 10:53:11 -0400

    Added framer motion package to help with animation

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {motion} from 'framer-motion';
 
 
 const Modal = ({selectedImg, setSelectedImg}) => {
@@ -11,10 +11,16 @@ const Modal = ({selectedImg, setSelectedImg}) => {
     }
 
     return (
-        <div className="backdrop" onClick={handleClick}>
-            <img src={selectedImg} alt="Enlarged state"></img>
+        <motion.div className="backdrop" onClick={handleClick}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+        >
+            <motion.img src={selectedImg} alt="Enlarged state"
+            initial= {{y: "-100vh"}}
+            animate= {{y: 0}}
+            ></motion.img>
 
-        </div>
+        </motion.div>
     )
 }
 
